86b08bb55df4c474c7671ba869f6e043833f426c
sopoour <sophia.auer@googlemail.com>
2021-09-20 18:48:43 +0200

    Adding conditional content to open and close new Expense form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
-    //function is called within the ExpenseForm when submitting the expense and gets the expenseData passed to it
-    const handleSaveExpense = (enteredExpenseData) => {
-        const expenseData = {
-            ...enteredExpenseData,
-            //add an id attribute to the object
-            id: Math.random().toString()
-        }
-        props.onAddExpense(expenseData);
-    }
+  //function is called within the ExpenseForm when submitting the expense and gets the expenseData passed to it
+  const handleSaveExpense = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      //add an id attribute to the object
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+    setIsOpen(false);
+  };
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenForm = () => {
+    setIsOpen(true);
+  };
+
+  const handleCloseForm = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={handleSaveExpense} />
+      {!isOpen && (
+        <button
+          style={{ textAlign: "center" }}
+          onClick={handleOpenForm}
+        >
+          Add New Expense
+        </button>
+      )}
+      {isOpen && (
+        <ExpenseForm
+          onCancel={handleCloseForm}
+          onSaveExpenseData={handleSaveExpense}
+        />
+      )}
     </div>
   );
 };
